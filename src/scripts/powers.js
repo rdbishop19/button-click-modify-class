1eb93e663f58bc389910bbb8235d4851800991cb
Ryan Bishop <rdbishop06@gmail.com>
2019-10-16 15:24:28 -0500

    Add first event listener

--- src/scripts/powers.js
+++ src/scripts/powers.js
@@ -1,1 +1,24 @@
-document.querySelector("#activate-flight").addEventListener()
+document.querySelector('#activate-flight').addEventListener('click', () => {
+	flightHandlerFunction();
+});
+
+// Remove 'disabled' class on <section id="flight"> and replace it with a class of 'enabled'.
+const flightHandlerFunction = () => {
+	let clickedEl = event.currentTarget.value;
+	console.log('Clicked activate flight button');
+    let flightClasses = document.querySelector('#flight');
+    flightClasses.classList.toggle('disabled');
+	flightClasses.classList.toggle('enabled');
+	console.log(flightClasses);
+};
+/* 
+Write two more JavaScript functions, and two more querySelector().addEventListener() statements 
+for handling the click event on the other two buttons. 
+Those event handlers should perform the same step of removing disabled, 
+and adding enabled on the corresponding <section> elements
+
+document.querySelector("#activate-mindreading").addEventListener()
+document.querySelector("#activate-xray").addEventListener()
+ */
+
+ 
