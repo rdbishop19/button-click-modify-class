9ebcb9c2ec97aea71232c3149b867db9817dd569
Ryan Bishop <rdbishop06@gmail.com>
2019-10-17 11:20:17 -0500

    feat: make buttons toggle vs activate and disable buttons based on logic

--- src/scripts/powers.js
+++ src/scripts/powers.js
@@ -1,3 +1,32 @@
+const buttonHandlerFunction = () => {
+    let powerStatus = document.getElementsByClassName("power")
+    let disabledCount = 0
+    let enabledCount = 0
+    for (let i = 0; i < powerStatus.length; i++){
+        // console.log(powerStatus[i].classList)
+        powerStatus[i].classList.forEach(function(className){
+            if (className === "disabled"){
+                disabledCount++
+            }
+            if (className === "enabled"){
+                enabledCount++
+            }
+        })
+        if (enabledCount === powerStatus.length){
+            document.getElementById("activate-all").disabled = true;
+        }
+        else {
+            document.getElementById("activate-all").disabled = false;            
+        }
+        if (disabledCount === powerStatus.length) {
+            document.getElementById("deactivate-all").disabled = true;
+        }
+        else {
+            document.getElementById("deactivate-all").disabled = false;
+        }
+    }
+}
+
 // one function to handle activating a power depending on which button is clicked
 const powerHandlerFunction = () => {
     let sectionClasses = event.target.id.split('-');
@@ -18,7 +47,7 @@ You will need to use the document.querySelectorAll() method for these.
  */
 const deactivateAllPowers = () => {
     let powerList = document.querySelectorAll("#powerList > section");
-    console.log(powerList);
+    // console.log(powerList);
     for (let i = 0; i < powerList.length; i++){
         powerList[i].classList.add('disabled');
         powerList[i].classList.remove('enabled');
@@ -27,7 +56,7 @@ const deactivateAllPowers = () => {
 
 const activateAllPowers = () => {
     let powerList = document.querySelectorAll("#powerList > section");
-    console.log(powerList);
+    // console.log(powerList);
     for (let i = 0; i < powerList.length; i++){
         powerList[i].classList.remove('disabled');
         powerList[i].classList.add('enabled');
@@ -36,3 +65,8 @@ const activateAllPowers = () => {
 
 document.querySelector('#deactivate-all').addEventListener('click', deactivateAllPowers);
 document.querySelector('#activate-all').addEventListener('click', activateAllPowers);
+window.onload = buttonHandlerFunction;
+const buttons = document.querySelectorAll("button");
+for (const button of buttons) {
+    button.addEventListener('click', buttonHandlerFunction)
+};
