0945bf6ac3c3475be8becc7416bac41fa8e47035
Ryan Bishop <rdbishop06@gmail.com>
2019-10-17 11:54:10 -0500

    refactor: internal function calls and event listeners

--- src/scripts/powers.js
+++ src/scripts/powers.js
@@ -1,9 +1,9 @@
+// check status of enabled/disabled powers and disable buttons accordingly
 const buttonHandlerFunction = () => {
     let powerStatus = document.getElementsByClassName("power")
     let disabledCount = 0
     let enabledCount = 0
     for (let i = 0; i < powerStatus.length; i++){
-        // console.log(powerStatus[i].classList)
         powerStatus[i].classList.forEach(function(className){
             if (className === "disabled"){
                 disabledCount++
@@ -27,45 +27,43 @@ const buttonHandlerFunction = () => {
     }
 }
 
-// one function to handle activating a power depending on which button is clicked
+// toggle individual power on/off
 const powerHandlerFunction = () => {
-    let sectionClasses = event.target.id.split('-');
-    let flightClasses = document.querySelector(`#${sectionClasses[1]}`);
-    flightClasses.classList.toggle('disabled');
-	flightClasses.classList.toggle('enabled');
+    let sectionClassNames = event.target.id.split('-');
+    let powerClassNames = document.querySelector(`#${sectionClassNames[1]}`).classList;
+    powerClassNames.toggle('disabled');
+	powerClassNames.toggle('enabled');
 };
 
+// add event listeners for individual power buttons
 document.querySelector('#activate-flight').addEventListener('click', powerHandlerFunction);
 document.querySelector('#activate-mindreading').addEventListener('click', powerHandlerFunction);
 document.querySelector('#activate-xray').addEventListener('click', powerHandlerFunction);
 
-/* 
-Write two more event handlers for activating and deactivating all powers 
-when the corresponding buttons are clicked. 
-
-You will need to use the document.querySelectorAll() method for these.
- */
-const deactivateAllPowers = () => {
-    let powerList = document.querySelectorAll("#powerList > section");
-    // console.log(powerList);
+// refactor deactivateAllPowers() and activateAllPowers() into one function
+const toggleAllPowers = () => {
+    console.log(event.target.id.split('-')[0]);
+    let buttonClicked = event.target.id.split('-')[0];
+    let powerList = document.querySelectorAll(".power");
     for (let i = 0; i < powerList.length; i++){
-        powerList[i].classList.add('disabled');
-        powerList[i].classList.remove('enabled');
+        if (buttonClicked === "activate") {
+            powerList[i].classList.remove('disabled');
+            powerList[i].classList.add('enabled');
+        }
+        else if (buttonClicked === "deactivate"){
+            powerList[i].classList.add('disabled');
+            powerList[i].classList.remove('enabled');
+        }
     }
 }
 
-const activateAllPowers = () => {
-    let powerList = document.querySelectorAll("#powerList > section");
-    // console.log(powerList);
-    for (let i = 0; i < powerList.length; i++){
-        powerList[i].classList.remove('disabled');
-        powerList[i].classList.add('enabled');
-    }
-}
+// listen for 'activate/deactive all' button clicks
+document.querySelector('#deactivate-all').addEventListener('click', toggleAllPowers);
+document.querySelector('#activate-all').addEventListener('click', toggleAllPowers);
 
-document.querySelector('#deactivate-all').addEventListener('click', deactivateAllPowers);
-document.querySelector('#activate-all').addEventListener('click', activateAllPowers);
+// check load state of powers enabled/disabled
 window.onload = buttonHandlerFunction;
+// check power state after each button click
 const buttons = document.querySelectorAll("button");
 for (const button of buttons) {
     button.addEventListener('click', buttonHandlerFunction)
